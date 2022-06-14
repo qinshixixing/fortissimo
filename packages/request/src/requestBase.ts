eb7d80b4250dac31019e05933f8ba00c8f596ffe
qinshixixing <qinshixixing@icloud.com>
2022-06-14 16:08:12 +0800

    feat(优化返回值处理逻辑): 返回值若没有传dataKey，则返回所有data数据

--- packages/request/src/requestBase.ts
+++ packages/request/src/requestBase.ts
@@ -67,7 +67,9 @@ export function requestBase(
   request.interceptors.response.use(
     (res: AxiosResponse) => {
       if (res.data && res.data[allConfig.codeKey] === allConfig.successCode) {
-        return Promise.resolve(res.data[allConfig.dataKey]);
+        return Promise.resolve(
+          allConfig.dataKey ? res.data[allConfig.dataKey] : res.data
+        );
       }
       console.error(res);
       return Promise.reject(setResError(res, false, allConfig));
