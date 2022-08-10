383e6b5eb4988673dae9a7f6b47ff97329ace5ae
qinshixixing <qinshixixing@icloud.com>
2022-08-10 19:21:02 +0800

    feat(优化setstoragemsgkey): 优化setStorageMsgKey

--- packages/hook/src/useStorageMessage.ts
+++ packages/hook/src/useStorageMessage.ts
@@ -1,13 +1,20 @@
 import { useCallback, useEffect } from 'react';
-import { parseJson } from '@fortissimo/util';
+import { parseJson, setStorageMsgKey } from '@fortissimo/util';
+import type { StorageMsgData } from '@fortissimo/util';
 
-export function useStorageMessage(callback: (key: string, value: any) => void) {
+export function useStorageMessage(
+  callback: (name: string, data: any, key: string) => void,
+  key?: string
+) {
   const listener = useCallback(
     (e: StorageEvent) => {
       if (e.newValue === null || e.key === null) return;
-      callback(e.key, parseJson(e.newValue));
+      const msgKey = key || setStorageMsgKey();
+      if (e.key !== msgKey) return;
+      const value: StorageMsgData = parseJson(e.newValue);
+      callback(value.name, value.data, e.key);
     },
-    [callback]
+    [callback, key]
   );
   useEffect(() => {
     window.addEventListener('storage', listener);
