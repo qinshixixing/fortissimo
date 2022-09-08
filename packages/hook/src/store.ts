2623071478ecb418399133534156f6acdcf577ca
qinshixixing <qinshixixing@icloud.com>
2022-09-08 17:37:42 +0800

    feat(优化store逻辑): 优化store逻辑

--- packages/hook/src/store.ts
+++ packages/hook/src/store.ts
@@ -3,27 +3,26 @@ import type { Dispatch, SetStateAction } from 'react';
 
 export type StoreData<T> = {
   state: T;
-  commit: {
-    [propName: string]: (state: T, data?: any) => Partial<T>;
-  };
-  dispatch: {
-    [propName: string]: (
+  commit: Record<string, (state: T, data?: any) => Partial<T>>;
+  dispatch: Record<
+    string,
+    (
       state: T,
       commit: Dispatch<{ type: string; [propName: string]: any }>,
       data?: any
-    ) => Promise<any>;
-  };
+    ) => Promise<any>
+  >;
 };
 
 export type Store<T> = {
   state: T;
   commit: Dispatch<{ type: string; [propName: string]: any }>;
-  dispatch: (data: any) => Promise<void>;
+  dispatch: (data: any) => Promise<any>;
 };
 
 export function useInitStore<T>(storeData: StoreData<T>): Store<T> {
   const [stateData, commitAction] = useReducer(
-    (state: T, data: { [propName: string]: any }) => {
+    (state: T, data: Record<string, any>) => {
       if (typeof storeData.commit[data.type] === 'function') {
         const commitData = { ...data };
         Reflect.deleteProperty(commitData, 'type');
